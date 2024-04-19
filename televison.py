1ebc36d4a2ba11da428ddf20c05f6dc5432eb305
teelorenzo <167545317+teelorenzo@users.noreply.github.com>
2024-04-19 11:59:21 -0500

    I am submitting this as instructed in LAB 12

--- televison.py
+++ televison.py
@@ -3,3 +3,46 @@ class Television:
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        self.__muted = not self.__muted
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status:
+            self.__muted = False
+        if self.__volume < Television.MAX_VOLUME:
+            self.__volume += 1
+
+    def volume_down(self):
+        if self.__status:
+            self.__muted = False
+        if self.__volume > Television.MIN_VOLUME:
+            self.__volume -= 1
+
+    def __str__(self):
+        return (f'Power = {self.__status},'
+                f'Channel = {self.__channel},'
+                f'Volume = {self.__volume}')
